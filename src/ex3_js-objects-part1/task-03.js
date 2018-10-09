25ede541257cd3203b77993fc727617c95d0e9e2
Naumov130793 <edn130793@mail.ru>
2018-10-09 21:10:57 +0300

    JS_Task_3

--- src/ex3_js-objects-part1/task-03.js
+++ src/ex3_js-objects-part1/task-03.js
@@ -1,9 +1,11 @@
 var f=function(str,obj){
     if((str in obj)==true){
         console.log("Свойство "+str+" существует в объекте");
+        return true;
     }
     else{
         console.log("Такого свойства нет! ");
+        return false;
     }
 }
 var obj={name:"Ivan", surname:"Ivanov", age:25, human:true};
