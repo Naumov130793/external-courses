ea77f6094ad5b2cd0fb0ff2c17d39a82bcecabad
Naumov130793 <43888431+Naumov130793@users.noreply.github.com>
2018-10-09 20:31:47 +0300

    Update task-06.js

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -2,6 +2,9 @@ var simpleNumber=function(a){
     if(a===0){
         console.log("Вы передали 0");
     }
+    if(a>=1000){
+        console.log("Вы ввели неверные данные");
+    }
     else if(typeof(a)==="number"){
         for(var i=2;i<=(a-1);i++){
             if((a%i)===0){
