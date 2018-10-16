650bb9fd34f1134c6697cefe7b13b66ede3fcda0
Naumov130793 <edn130793@mail.ru>
2018-10-16 18:58:41 +0300

    task-05

---
+++ src/ex3_js-objects-part1/task-05.js
@@ -0,0 +1,10 @@
+var f=function(obj){
+    var copyObj=obj;
+    return copyObj;
+    // console.log(copyObj);
+    }
+    
+    var obj={name:"Ivan", surname:"Ivanov",age:25,city:"Ryazan"};
+    //f(obj);
+    console.log(f(obj));
+    console.log(obj); 
