5f5d701de1e821eaccce6df77664318a77c3564d
Eduard Naumov <edn130793@mail.ru>
2018-10-24 21:11:02 +0300

    task-04

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,18 +1,13 @@
 var f=function(arr){
-    var l=arr.length-1;
-    var flag=0;
-    for(var i=0;i<l;i++){
-        if(arr[i]===arr[i+1]){
-            ++flag
-        }
-    }
-    if(flag===l){
-        console.log("Все элементы массива равны");
-    }
-    else{
-        console.log("Не все элементы массива равны");    
-    }
-}
 
-var arr=[3,3,3,3,3];
-f(arr);
+    for(var i=0;i<arr.length;i++){
+    for(var j=1;j<arr.length;j++){
+    if (arr[i]!==arr[j]){
+    return false;
+    }
+    }
+    }
+    return true;
+    }
+    var arr=[2,2,2,2,2,2,2,2];
+    console.log(f(arr));
